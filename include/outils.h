0e8b0472f3c677e3427412107026ec1fc797592c
ruscot <thonymartiinez@gmail.com>
2020-02-04 18:01:02 +0100

    mauvais TP3

--- include/outils.h
+++
@@ -1,8 +0,0 @@
-#ifndef _OUTILS_H_
-#define _OUTILS_H_
-#include "messages.h"
-
-void LireDouble(char *phrase, double *nombre);
-void LireShort(char *phrase, short *nombre); 
-
-#endif
--- lib_messages/include/messages.h
+++
@@ -1,9 +0,0 @@
-#ifndef _MESSAGES_H_
-#define _MESSAGES_H_
-
-#define CONTINUER 1
-#define SORTIR 0
-
-void Message(char *message, short etat);
-
-#endif
